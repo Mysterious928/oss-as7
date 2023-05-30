a3bf8d7b1ff2bd21a3d1ebf2acb96ad53bbfe222
Ablblkv <iliasmr765@gmail.com>
2023-05-30 23:04:03 +0600

    Update script.py

--- script.py
+++ script.py
@@ -22,25 +22,11 @@ def service_enum(target, port):
         print(f"An error occurred during service enumeration: {e}")
 
 
-def find_directories(target, wordlist_file):
-    try:
-        print("Running gobuster on target {}".format(target))
-
-        subprocess.run(['gobuster', 'dir', '-u', target, '-w', wordlist_file], text=True)
-    except Exception as e:
-        print(f"An error occurred during gobuster execution: {e}")
-
-
 parser = argparse.ArgumentParser(description="A simple pentest script.")
 parser.add_argument("-t", "--target", required=True, help="Target IP address.")
 parser.add_argument("-p", "--ports", nargs="+", default=[22, 80, 443], type=int, help="Ports to scan.")
-parser.add_argument("-a","--all-ports", action="store_true", help="Scan all ports (overrides --ports).")
-parser.add_argument("-w", "--wordlist-file", type=argparse.FileType('r'), required=True, help="File with wordlist for gobuster.")
 
 args = parser.parse_args()
-
-if args.all_ports:
-    args.ports = range(1, 65536)
 
 open_ports = []
 try:
@@ -53,8 +39,6 @@ try:
 
     for port in open_ports:
         service_enum(args.target, port)
-
-    find_directories(args.target, args.wordlist_file.name)
 
 except KeyboardInterrupt:
     print("\nExiting...")

e290a34d505860636213caf3ced52e32b0547066
Ablblkv <iliasmr765@gmail.com>
2023-05-30 22:44:00 +0600

    hello
    DESCRIPTION
    
    This Python script offers a basic penetration testing tool. It can scan specific ports on a target IP address, enumerate services on open ports using nmap, and perform directory brute-forcing using gobuster.
    
    REQUIREMENTS
    
    To successfully run the script, ensure that you have Python 3 installed, along with the following dependencies:
    
    - nmap: This tool is necessary for enumerating services on open ports.
    - gobuster: This tool is required for directory brute forcing.
    
    Before executing the script, verify that these dependencies are installed and accessible from your system's PATH.
    
    USAGE
    
    In order to utilize the script, you must supply a target IP address, specify a list of ports for scanning, and provide a wordlist file to be used by gobuster.
    
    The available command-line arguments are as follows:
    ●	-t, --target: Target IP address. (required)
    ●	-p, --ports: Ports to scan. Default are ports 22, 80, and 443. (optional)
    ●	-a, --all-ports: Scan all ports. (optional)
    ●	-w, --wordlist-file: File path to the wordlist for gobuster. (required)
    Here is an example of how to run the script:
    	python gbscan.py -t <target_IP> -p <ports> -w <path_to_wordlist>

---
+++ script.py
@@ -0,0 +1,65 @@
+import argparse
+import concurrent.futures
+import socket
+import subprocess
+
+def port_scan(target, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(1)
+        result = sock.connect_ex((target,port))
+        if result == 0:
+            print("Port {} is open.".format(port))
+            return port
+
+
+def service_enum(target, port):
+    try:
+        print("Enumerating port {}:".format(port))
+
+        result = subprocess.check_output(['nmap', '-sV', '-p', str(port), target])
+        print(result.decode('utf-8'))
+    except Exception as e:
+        print(f"An error occurred during service enumeration: {e}")
+
+
+def find_directories(target, wordlist_file):
+    try:
+        print("Running gobuster on target {}".format(target))
+
+        subprocess.run(['gobuster', 'dir', '-u', target, '-w', wordlist_file], text=True)
+    except Exception as e:
+        print(f"An error occurred during gobuster execution: {e}")
+
+
+parser = argparse.ArgumentParser(description="A simple pentest script.")
+parser.add_argument("-t", "--target", required=True, help="Target IP address.")
+parser.add_argument("-p", "--ports", nargs="+", default=[22, 80, 443], type=int, help="Ports to scan.")
+parser.add_argument("-a","--all-ports", action="store_true", help="Scan all ports (overrides --ports).")
+parser.add_argument("-w", "--wordlist-file", type=argparse.FileType('r'), required=True, help="File with wordlist for gobuster.")
+
+args = parser.parse_args()
+
+if args.all_ports:
+    args.ports = range(1, 65536)
+
+open_ports = []
+try:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        futures = {executor.submit(port_scan, args.target, port) for port in args.ports}
+        for future in concurrent.futures.as_completed(futures):
+            port = future.result()
+            if port is not None:
+                open_ports.append(port)
+
+    for port in open_ports:
+        service_enum(args.target, port)
+
+    find_directories(args.target, args.wordlist_file.name)
+
+except KeyboardInterrupt:
+    print("\nExiting...")
+    try:
+        for future in futures:
+            future.cancel()
+    except:
+        pass
